32b4ebb29797cf394cfa273e6072402cc1a4520a
Mitch <ndianguimichelle@gmail.com>
2025-02-13 23:34:34 +0300

    feat: maintaining the user's tab

--- src/components/TicketForm.tsx
+++ src/components/TicketForm.tsx
@@ -7,6 +7,18 @@ import Ready from "./Ready";
 export default function TicketForm(){
   const [tab, setTab] = useState(1);
 
+  useEffect(() => {
+    if(localStorage.getItem('tab') && localStorage.getItem('tab') !== 'NaN'){
+      setTab(Number(localStorage.getItem('tab')));
+    }
+  }, []);
+
+  const changeTab = (i: number) => {
+    setTab(i);
+    localStorage.setItem('tab', `${i}`);
+  }
+
+
   return (
     <section className="w-full h-full flex items-center justify-center">
     { tab < 3 && (<section className="w-full p-[24px] max-w-[700px] lg:left-[370px] lg:right-[370px] flex flex-col gap-[32px] lg:p-[48px] bg-[#041e23] border-[1px] border-[#0e464f] rounded-[32px] lg:rounded-[40px] mx-auto md:mt-[70px] lg:mt-[40px] mt-[40px] h-fit">
@@ -20,16 +32,16 @@ export default function TicketForm(){
       }
       {
         tab === 1 && (
-          <TicketSelection  setTab={setTab}  />
+          <TicketSelection  setTab={changeTab}  />
         )
       }
       {
         tab === 2 && (
-          <AttendeeDetails setTab={setTab} />
+          <AttendeeDetails setTab={changeTab} />
         )
       }
     </section>)}
-    { tab === 3 && (<Ready setTab={setTab} />) }
+    { tab === 3 && (<Ready setTab={changeTab} />) }
     </section>
   );
 }
